028532929cf6256df2f0cb6665b898800df89a0b
pefish <pefish@qq.com>
2020-11-03 11:22:41 +0800

    add

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"create_golang_app_template/pkg/command"
+	"create_golang_app_template/version"
+	"github.com/pefish/go-commander"
+	"log"
+)
 
 func main() {
-	fmt.Println(`Hello World!!`)
+	commanderInstance := commander.NewCommander(version.AppName, version.Version, "template 是一个模板，祝你玩得开心。作者：pefish")
+	//commanderInstance.RegisterSubcommand("client", client.NewClient())
+	//commanderInstance.RegisterSubcommand("server", server.NewServer())
+	commanderInstance.RegisterDefaultSubcommand(command.NewDefaultCommand())
+	err := commanderInstance.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module create_golang_app_template
 
 go 1.14
+
+require (
+	github.com/pefish/go-commander v0.2.2
+	github.com/pefish/go-config v0.2.1
+	github.com/pefish/go-logger v0.4.3
+)
