2ad417c718ddd2898c3e4e71a767b6d23873d9f3
pefish <pefish@qq.com>
2020-12-07 17:31:55 +0800

    upgrade

--- cmd/main/command/default.go
+++ cmd/main/command/default.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"flag"
+	"github.com/pefish/go-commander"
 	go_config "github.com/pefish/go-config"
 	go_logger "github.com/pefish/go-logger"
 	"net"
@@ -26,7 +27,7 @@ func (dc *DefaultCommand) OnExited() error {
 	return nil
 }
 
-func (dc *DefaultCommand) Start() error {
+func (dc *DefaultCommand) Start(data commander.StartData) error {
 	tcpAddress, err := go_config.Config.GetString("tcp-address")
 	if err != nil {
 		return err
