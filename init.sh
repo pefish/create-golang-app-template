e4de75baa12825109a68d7375ea904afe7eeea5a
pefish <darrondarron@qq.com>
2023-10-25 18:20:33 +0800

    add

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 cat go.mod | sed "s@create_golang_app_template@${NAME}@g" > temp && rm -rf go.mod && mv temp go.mod
+
+cat README.md | sed "s@XXX@${PROJECT_NAME}@g" > temp && rm -rf README.md && mv temp README.md
+
+cat README_zh-cn.md | sed "s@XXX@${PROJECT_NAME}@g" > temp && rm -rf README_zh-cn.md && mv temp README_zh-cn.md
+
